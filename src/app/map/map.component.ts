b008ea703aadbedf9aefab94ccc3e3bea12db254
UnicornBuster <symasp@gmail.com>
2018-06-04 17:21:44 +0200

    Affiche les images de Google street view

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,19 +11,33 @@ export class MapComponent implements OnInit {
   constructor(private _localRestaurantsService: LocalRestaurantsService) { }
 
   localRestaurants:any = [];
+  neLat;
+  neLng;
+  swLat;
+  swLng;
 
   ngOnInit() {
     // Geo Location
     this.findMe()
+
     this._localRestaurantsService.getRestaurants().subscribe(
       data => this.localRestaurants = data
     );
+  }
+
+  boundsChange(latLngBounds: LatLngBounds): void {
+    //console.log('bounds changed ' + latLngBounds);
+    this.neLat = latLngBounds.getNorthEast().lat();
+    this.neLng = latLngBounds.getNorthEast().lng();
+    this.swLat = latLngBounds.getSouthWest().lat();
+    this.swLng = latLngBounds.getSouthWest().lng();
   }
 
   // Paris Coords
   lat: number = 48.855314;
   lng: number = 2.345883;
   locationFound = false;
+
 
 
 
@@ -45,6 +59,24 @@ export class MapComponent implements OnInit {
     } else {
       alert("Geolocation is not supported by this browser.");
     }
+  }
+
+  private isVisible(i){
+
+    if (this.localRestaurants[i].lat < this.neLat && this.localRestaurants[i].lat > this.neLng){
+      //console.log('oooo');
+      return true;
+    } else {
+      console.log('NOP');
+      return false;
+    }
+
+    /*if(this.getBounds().contains(marker.getPosition())){
+      return true;
+    } else {
+      return false;
+    }*/
+
   }
 
 }
