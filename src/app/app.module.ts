c06bd0f68b1d9cca3b14f67b2a5c1bd3541690b8
UnicornBuster <symasp@gmail.com>
2018-06-05 16:48:13 +0200

    angular-star-rating installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
 import { HttpModule } from '@angular/http';
+import { StarRatingModule } from 'angular-star-rating';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
@@ -18,6 +19,7 @@ import { RestaurantListComponent } from './restaurant-list/restaurant-list.compo
   imports: [
     BrowserModule,
     HttpModule,
+    StarRatingModule.forRoot(),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCY2YSw8RU-njyTNebCy0-FlH33LleOt-4'
     })
